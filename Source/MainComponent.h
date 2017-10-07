b200dcf13e3f2e600f65bb897a4e7809dfa5e4df
idearcos <idearcos@users.noreply.github.com>
2017-10-07 16:01:07 +0900

    Removed lots of code we are not going to use.

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -27,215 +27,45 @@
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "LightpadComponent.h"
 
-//==============================================================================
-/**
-    A struct that handles the setup and layout of the DrumPadGridProgram
-*/
-struct ColourGrid
-{
-    ColourGrid (int cols, int rows)
-        : numColumns (cols),
-          numRows (rows)
-    {
-        constructGridFillArray();
-    }
-
-    /** Creates a GridFill object for each pad in the grid and sets its colour
-        and fill before adding it to an array of GridFill objects
-    */
-    void constructGridFillArray()
-    {
-        gridFillArray.clear();
-
-        auto counter = 0;
-
-        for (auto i = 0; i < numColumns; ++i)
-        {
-            for (auto j = 0; j < numRows; ++j)
-            {
-                DrumPadGridProgram::GridFill fill;
-                Colour colourToUse = colourArray.getUnchecked (counter);
-
-                fill.colour = colourToUse.withBrightness (colourToUse == currentColour ? 1.0f : 0.1f);
-
-                if (colourToUse == Colours::black)
-                    fill.fillType = DrumPadGridProgram::GridFill::FillType::hollow;
-                else
-                    fill.fillType = DrumPadGridProgram::GridFill::FillType::filled;
-
-                gridFillArray.add (fill);
-
-                if (++counter == colourArray.size())
-                    counter = 0;
-            }
-        }
-    }
-
-    /** Sets which colour should be active for a given touch co-ordinate. Returns
-        true if the colour has changed
-    */
-    bool setActiveColourForTouch (int x, int y)
-    {
-        auto colourHasChanged = false;
-
-        auto xindex = x / 5;
-        auto yindex = y / 5;
-
-        auto newColour = colourArray.getUnchecked ((yindex * 3) + xindex);
-        if (currentColour != newColour)
-        {
-            currentColour = newColour;
-            constructGridFillArray();
-            colourHasChanged = true;
-        }
-
-        return colourHasChanged;
-    }
-
-    //==============================================================================
-    int numColumns, numRows;
-
-    Array<DrumPadGridProgram::GridFill> gridFillArray;
-
-    Array<Colour> colourArray = { Colours::white, Colours::red, Colours::green,
-                                  Colours::blue, Colours::hotpink, Colours::orange,
-                                  Colours::magenta, Colours::cyan, Colours::black };
-
-    Colour currentColour = Colours::hotpink;
-
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ColourGrid)
-};
-
-//==============================================================================
-/**
-    The main component
-*/
-class MainComponent   : public Component,
-                        public TopologySource::Listener,
-                        private TouchSurface::Listener,
-                        private ControlButton::Listener,
-                        private LightpadComponent::Listener,
-                        private Button::Listener,
-                        private Slider::Listener,
-                        private Timer
+class MainComponent : public Component,
+	public TopologySource::Listener,
+	private TouchSurface::Listener,
+	private ControlButton::Listener
 {
 public:
-    MainComponent();
-    ~MainComponent();
+	MainComponent();
+	~MainComponent();
 
-    void resized() override;
+	void resized() override;
 
-    /** Overridden from TopologySource::Listener. Called when the topology changes */
-    void topologyChanged() override;
+	/** Overridden from TopologySource::Listener. Called when the topology changes */
+	void topologyChanged() override;
 
 private:
-    /** Overridden from TouchSurface::Listener. Called when a Touch is received on the Lightpad */
-    void touchChanged (TouchSurface&, const TouchSurface::Touch&) override;
+	/** Overridden from TouchSurface::Listener. Called when a Touch is received on the Lightpad */
+	void touchChanged(TouchSurface&, const TouchSurface::Touch&) override;
 
-    /** Overridden from ControlButton::Listener. Called when a button on the Lightpad is pressed */
-    void buttonPressed (ControlButton&, Block::Timestamp) override { }
+	/** Overridden from ControlButton::Listener. Called when a button on the Lightpad is pressed */
+	void buttonPressed(ControlButton&, Block::Timestamp) override { }
 
-    /** Overridden from ControlButton::Listener. Called when a button on the Lightpad is released */
-    void buttonReleased (ControlButton&, Block::Timestamp) override;
+	/** Overridden from ControlButton::Listener. Called when a button on the Lightpad is released */
+	void buttonReleased(ControlButton&, Block::Timestamp) override;
 
-    void ledClicked (int x, int y, float z) override;
+	/** Removes TouchSurface and ControlButton listeners and sets activeBlock to nullptr */
+	void detachActiveBlock();
 
-    void buttonClicked (Button*) override;
+	/** Sets an LED on the Lightpad for a given touch co-ordinate and pressure */
+	void drawLED(uint32 x0, uint32 y0, float z, Colour drawColour);
 
-    void sliderValueChanged (Slider*) override;
+	/** Redraws the LEDs on the Lightpad from the activeLeds array */
+	void redrawLEDs();
 
-    void timerCallback() override;
+	PhysicalTopologySource topologySource;
+	Block::Ptr lightpad_block_;
 
-    /** Removes TouchSurface and ControlButton listeners and sets activeBlock to nullptr */
-    void detachActiveBlock();
+	Label infoLabel;
 
-    /** Sets the LEDGrid Program for the selected mode */
-    void setLEDProgram (Block&);
-
-    void clearLEDs();
-
-    /** Sets an LED on the Lightpad for a given touch co-ordinate and pressure */
-    void drawLED (uint32 x0, uint32 y0, float z, Colour drawColour);
-
-    /** Redraws the LEDs on the Lightpad from the activeLeds array */
-    void redrawLEDs();
-
-    //==============================================================================
-    BitmapLEDProgram* getCanvasProgram()
-    {
-        if (activeBlock != nullptr)
-            return dynamic_cast<BitmapLEDProgram*> (activeBlock->getProgram());
-
-        return nullptr;
-    }
-
-    DrumPadGridProgram* getPaletteProgram()
-    {
-        if (activeBlock != nullptr)
-            return dynamic_cast<DrumPadGridProgram*> (activeBlock->getProgram());
-
-        return nullptr;
-    }
-
-    //==============================================================================
-    /**
-        A struct that represents an active LED on the Lightpad.
-        Has a position, colour and brightness.
-    */
-    struct ActiveLED
-    {
-        uint32 x, y;
-        Colour colour;
-        float brightness;
-
-        /** Returns true if this LED occupies the given co-ordinates */
-        bool occupies (uint32 xPos, uint32 yPos) const
-        {
-            return xPos == x && yPos == y;
-        }
-    };
-    Array<ActiveLED> activeLeds;
-
-    int getLEDAt (uint32 x, uint32 y) const
-    {
-        for (int i = 0; i < activeLeds.size(); ++i)
-            if (activeLeds.getReference(i).occupies (x, y))
-                return i;
-
-        return -1;
-    }
-
-    //==============================================================================
-    enum DisplayMode
-    {
-        colourPalette = 0,
-        canvas
-    };
-    DisplayMode currentMode = colourPalette;
-
-    //==============================================================================
-    ColourGrid layout { 3, 3 };
-    PhysicalTopologySource topologySource;
-    Block::Ptr activeBlock;
-
-    float scaleX = 0.0;
-    float scaleY = 0.0;
-
-    bool doublePress = false;
-
-    Label infoLabel;
-    LightpadComponent lightpadComponent;
-    TextButton clearButton;
-    LEDComponent brightnessLED;
-    Slider brightnessSlider;
-
-   #if JUCE_IOS
-    TextButton connectButton;
-   #endif
-
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+	//==============================================================================
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
